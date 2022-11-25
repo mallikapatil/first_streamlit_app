fe07c73f152901e0d756552b50dc362808af8df3
Mallika Patil <118974092+mallikapatil@users.noreply.github.com>
2022-11-25 11:57:22 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My first program in streamlit')
 
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Menu :-)')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
