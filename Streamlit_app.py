95c746227812828754b5d1ba7dcc16b2fa174447
Mallika Patil <118974092+mallikapatil@users.noreply.github.com>
2022-11-25 11:54:53 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,11 +1,9 @@
 import streamlit
-streamlit.title('My first program in streamlit')
+import emoji
 
+streamlit.title('My first program in streamlit')
 streamlit.header('Breakfast Menu')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
-
-
-
+streamlit.text('Hard-Boiled Free-Range Egg') 
