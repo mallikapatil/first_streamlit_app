163cf98ada16f20a4b7ee7db6b4b3a8074ae1234
Mallika Patil <118974092+mallikapatil@users.noreply.github.com>
2022-11-25 12:16:25 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit
+import pandas
 
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 streamlit.title('My first program in streamlit')
 
@@ -8,3 +11,6 @@ streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg') 
+
+streamlit.dataframe(my_fruit_list)
+
